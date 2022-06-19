8db39dad7d7315f5ba71a40dc24ab061cf8e25f9
Borhan Rabbani <borhanuddinse28@gmail.com>
2022-06-19 20:09:49 +0600

    Add files via upload
    https://leetcode.com/problems/valid-palindrome/submissions/

---
+++ ValidPalindrome.java
@@ -0,0 +1,39 @@
+package com.test;
+
+public class ValidPalindrome {
+
+	public static void main(String[] args) {
+		String s = "race a car";
+		char[] arr = new char[s.length()];
+		arr = s.toCharArray();
+		int counter = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char temp = Character.toLowerCase(arr[i]);
+//			System.out.println(temp);
+			if ((temp >= 'a' && temp <= 'z')) {
+				arr[counter] = temp;
+				counter++;
+			}
+			if(temp>=48 && temp<=57) {
+				arr[counter] = temp;
+				counter++;
+			}
+		}
+		boolean state = true;
+		System.out.println(counter);
+
+		int i = 0, j = counter - 1;
+
+		while (i < j) {
+			if (arr[i] != arr[j]) {
+				state = false;
+				break;
+			}
+			i++;
+			j--;
+		}
+
+		System.out.println(state);
+	}
+
+}
