ce6475ea302861ec60a13d61de8ffffd24178892
Borhan Rabbani <borhanuddinse28@gmail.com>
2022-07-01 00:10:00 +0600

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/missing-number/submissions/
+
 package com.math;
 
 import java.util.HashSet;
@@ -8,6 +10,8 @@ public class MissingNumber {
 	public static void main(String[] args) {
 		int[] nums = {9,6,4,2,3,5,7,0,1};
 		
+		// Using HashSet, this problem can be solved, but this approach take extra space because of HashSet
+		// This is why below approach is less efficient.
 		Set<Integer> set = new HashSet<Integer>();
 		for(int i: nums) {
 			set.add(i);
@@ -20,6 +24,18 @@ public class MissingNumber {
 			}
 		}
 		System.out.println(missing);
+		
+		// Below approach is more efficient and doesn't need extra space
+		int givenCount=0, resultCount=0;
+		
+		for(int j=0; j<nums.length; j++) {
+			givenCount += nums[j];
+			resultCount += j;
+		}
+		resultCount += nums.length;
+		int temp = resultCount - givenCount;
+		
+		System.out.println(temp);
 	}
 
 }
