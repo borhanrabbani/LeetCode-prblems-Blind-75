21b961565d2cbc3ecf29b6464f8d0ebbef93699e
Borhan Rabbani <borhanuddinse28@gmail.com>
2022-07-04 18:39:56 +0600

    Add files via upload

---
+++ ValidParentheses.java
@@ -0,0 +1,38 @@
+package com.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		Map<Character, Character> hm = new HashMap<Character, Character>();
+		
+		hm.put(')', '(');
+		hm.put('}', '{');
+		hm.put(']', '[');
+		
+		for(char c: s.toCharArray()) {
+			if(hm.containsKey(c)) {
+				if(!stack.isEmpty() && stack.peek()==hm.get(c)) {
+					stack.pop();
+				}else {
+					return false;
+				}
+			}else {
+				stack.push(c);
+			}
+		}
+		
+		return stack.isEmpty();
+
+	}
+
+	public static void main(String[] args) {
+		ValidParentheses vp = new ValidParentheses();
+		System.out.println(vp.isValid("{{}[][[[]]]}"));
+	}
+
+}
