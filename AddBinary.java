82ef3cef9ed513e5752568e606875e11a90f4448
Borhan Rabbani <borhanuddinse28@gmail.com>
2023-05-29 22:52:37 +0600

    Add files via upload

---
+++ AddBinary.java
@@ -0,0 +1,42 @@
+package neetcode;
+
+public class AddBinary {
+	
+	public String addBinary(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		
+		int carry = 0;
+		
+		int i=a.length()-1;
+		int j=b.length()-1;
+		
+		while(i>=0 || j>=0) {
+			int sum = carry;
+			
+			if(i>=0) {
+				sum += a.charAt(i)-'0';
+			}
+			if(j>=0) {
+				sum += b.charAt(j)-'0';
+			}
+			
+			sb.append(sum%2);
+			carry = sum/2;
+			i--;
+			j--;
+		}
+		
+		if(carry !=0)
+			sb.append(carry);
+		
+		return sb.reverse().toString();
+		
+	}
+
+	public static void main(String[] args) {
+		
+		AddBinary ab = new AddBinary();
+		System.out.println(ab.addBinary("1111", "1010"));
+	}
+
+}
